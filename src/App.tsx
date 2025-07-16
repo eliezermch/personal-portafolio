261cfa3cbeda6b04a9d55b614618fa6e800c926a
Eliezer Chirino <emchch3001@gmail.com>
2025-07-16 15:49:50 -0300

    New portfolio - initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+
+import { Canvas } from '@react-three/fiber';
+import './App.css'
+import { Particles } from './components/Particles';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Canvas
+            // scene={{ background: cubeTexture }}
+            camera={{
+              fov: 45,
+              near: 0.1,
+              far: 4000,
+              position: [-50, 320, 500],
+            }}
+            className='w-full h-full'
+            onCreated={(state) => {
+              const _gl = state.gl.getContext();
+              const pixelStorei = _gl.pixelStorei.bind(_gl);
+              _gl.pixelStorei = function (...args) {
+                const [parameter] = args;
+                switch (parameter) {
+                  case _gl.UNPACK_FLIP_Y_WEBGL:
+                    return pixelStorei(...args);
+                }
+              };
+            }}
+          >
+
+            <Particles />
+
+            <ambientLight intensity={0.5} />
+          </Canvas>
+    </>
+  )
+}
+
+export default App
